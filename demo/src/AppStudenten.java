fc27b2ae83b6f7730967cd9bf12945453576ccf1
Richard Jansen <richard@lrtbl.nl>
2015-09-24 11:48:31 +0200

    demo les 5

---
+++ demo/src/AppStudenten.java
@@ -0,0 +1,11 @@
+/**
+ * Created by richard on 9/24/15.
+ */
+public class AppStudenten {
+
+    public static void main(String[] args) {
+        Student een = new Student();
+        een.setLeeftijd(17);
+        een.naam = "Pepijn";
+    }
+}
---
+++ demo/src/demo.java
@@ -0,0 +1,26 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by richard on 9/24/15.
+ */
+public class demo {
+    private int leeftijd;
+    String naam;
+    private static final int minLeeftijd = 16;
+
+    public void info() {
+        System.out.println("Student: "+naam);
+        System.out.println("Leeftijd: "+Integer.toString(leeftijd));
+    }
+
+    public void setLeeftijd(int 1) {
+        if (1 < minLeeftijd) {
+            System.out.println("Dat mag niet!");
+        }else{
+            leeftijd = 1;
+        }
+    }
+    public static void setMinLeeftijd (int i){
+        minLeeftijd = i;
+    }
+}
